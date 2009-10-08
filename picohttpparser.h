c2d0ac6833cec3b9fcc54cab7cb3f69bc5f2379b
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-10-08 11:54:16 +0900

    work with c++

--- picohttpparser.h
+++ picohttpparser.h
@@ -1,6 +1,10 @@
 #ifndef picohttpparser_h
 #define picohttpparser_h
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* contains name and value of a header (name == NULL if is a continuing line
  * of a multiline header */
 struct phr_header {
@@ -23,5 +27,9 @@ int phr_parse_response(const char* _buf, size_t len, int *minor_version,
               int *status, const char **msg, size_t *msg_len,
               struct phr_header* headers, size_t* num_headers,
               size_t last_len);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
